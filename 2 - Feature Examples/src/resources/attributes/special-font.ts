55cc5a111a9c15f2c93a285813bc7bcc5e615835
Andreas Wänqvist <andrkarl@gmail.com>
2017-01-29 00:56:59 +0100

    fix

--- 2 - Feature Examples/src/resources/attributes/special-font.ts
+++ 2 - Feature Examples/src/resources/attributes/special-font.ts
@@ -2,24 +2,26 @@ import { autoinject } from 'aurelia-framework';
 
 @autoinject()
 export class SpecialFontCustomAttribute {
+
   constructor(private element: Element) {
-    (<any>this.element).style.fontFamily = 'Comic Sans MS';
-    (<any>this.element).style.fontSize = '102px';
-    (<any>this.element).style.width = '48%';
-    (<any>this.element).style.display = 'inline-block';
-    (<any>this.element).style.backgroundImage = `-webkit-gradient(
+    const htmlElement = this.element as HTMLElement;
+    htmlElement.style.fontFamily = 'Comic Sans MS';
+    htmlElement.style.fontSize = '102px';
+    htmlElement.style.width = '48%';
+    htmlElement.style.display = 'inline-block';
+    htmlElement.style.backgroundImage = `-webkit-gradient(
     linear,
     left top,
     right top,
     color-stop(0.03, rgb(250,3,3)),
     color-stop(0.52, rgb(240,255,127)),
     color-stop(0.76, rgb(42,24,173)))`;
-    (<any>this.element).style.webkitBackgroundClip= 'text';
-    (<any>this.element).style.webkitTextFillColor='transparent';
+    htmlElement.style.webkitBackgroundClip = 'text';
+    htmlElement.style.webkitTextFillColor = 'transparent';
+    htmlElement.style.transform = 'rotate(-45deg)';
   }
 
-  valueChanged(newValue, oldValue) {
+  valueChanged(newValue, oldValue) { }
 
-  }
 }
 
